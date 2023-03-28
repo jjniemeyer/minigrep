5fa0bef0a596b7094fe22c0637631c699538ca8f
Jasper Niemeyer <jani5714@colorado.edu>
2023-03-28 15:56:49 -0600

    save arg values into variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    // Discarding &args[0] for the purpose of this simple program
+    let query = &args[1];
+    let file_path = &args[2];
+   
+    // sanity checks
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
