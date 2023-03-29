d43357c1731dbf8666257c3b9a0f4dde24486c87
Jasper Niemeyer <jani5714@colorado.edu>
2023-03-29 13:31:42 -0600

    pass ownership of args iter directly to Config::build

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ impl Config {
         let file_path = args[2].clone();
        
         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config { query, file_path, ignore_case })
     }
 }
