d7f40a28f746a65f6b10fa4b867d4d9c8ab9999f
Jasper Niemeyer <jani5714@colorado.edu>
2023-03-29 09:39:58 -0600

    make search_case_insensitive pass the test

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // convert query to lowercase and search against each line converted to lowercase
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,9 +75,10 @@ Duct tape.";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick Three";
+Pick Three
+Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 
 }
